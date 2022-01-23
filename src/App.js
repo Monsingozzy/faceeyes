9feb95c4b5cd11ba0a0054c597418c9cec99af4f
Monsingozzy <mohansinghyoyo88@gmail.com>
2022-01-23 10:33:47 +0530

    mn

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ const App = () => {
               className="login-button"
               onClick={submitdata}
               style={{
-                width: "270px",
+                width: "250px",
                 height: "48px",
                 backgroundColor: "blue",
                 color: "white",
